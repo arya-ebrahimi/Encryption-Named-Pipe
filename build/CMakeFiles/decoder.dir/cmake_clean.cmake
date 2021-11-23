7f9e6fbb83993b73e8901e36b01ea15ca4ac619f
arya <aryaebrahimi3000@gmail.com>
2021-11-23 16:55:15 +0330

    second

---
+++ build/CMakeFiles/decoder.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/decoder.dir/decoder.c.o"
+  "decoder"
+  "decoder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/decoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/finder.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/finder.dir/finder.c.o"
+  "finder"
+  "finder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/finder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
